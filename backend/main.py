c20070cce6d8a78cff8a98c4db86522b7593fb0b
Ujwol Aryal <ujwolaryal@gmail.com>
2026-01-22 12:07:19 +0545

    Add dotenv support to load environment variables

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os 
 
 app = FastAPI()
+
+load_dotenv()
+
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+
 
 @app.get("/")
 def root():
